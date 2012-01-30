10aec9b70c7a097457230560e2aebdb30b153a8f
jangboa <korigp21@miraesoft.kr>
2012-01-30 19:57:22 +0900

    Author CRUD view

---
+++ src/main/java/kr/miraesoft/miraebook/repository/AuthorRepositoryNew.java
@@ -0,0 +1,9 @@
+package kr.miraesoft.miraebook.repository;
+
+import kr.miraesoft.miraebook.domain.Author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepositoryNew extends JpaRepository<Author, Integer>{
+
+}
