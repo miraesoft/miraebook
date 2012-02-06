fa6b3d3222c5eedd9df05ac4751c69103f0206c4
ahnsoyoung <sosimisy@miraesoft.kr>
2012-02-06 13:05:16 +0900

    저자 Author CRUD view

--- src/main/java/kr/miraesoft/miraebook/service/AuthorService.java
+++ src/main/java/kr/miraesoft/miraebook/service/AuthorService.java
@@ -1,11 +1,20 @@
 package kr.miraesoft.miraebook.service;
 
+import java.util.List;
+
 import kr.miraesoft.miraebook.domain.Author;
 
 public interface AuthorService {
 
-	void addAuthor(Author author);
+	Author addAuthor(Author author);
 
-	Author getAuthor(Author author);
+	Author getAuthor(Integer no);
+
+	List<Author> getAuthorList();
+
+	void deleteAuthor(Integer no);
+
+	Author updateAuthor(Author author);
+
 
 }
