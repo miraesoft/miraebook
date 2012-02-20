5e43e9f2cde354d32e85f6db9b5bc183672c0bfe
안병휘 <haist2002@gmail.com>
2012-02-20 21:48:15 +0900

    북 컨트롤러 진행중

--- src/main/java/kr/miraesoft/miraebook/controller/BookController.java
+++ src/main/java/kr/miraesoft/miraebook/controller/BookController.java
@@ -1,5 +1,32 @@
 package kr.miraesoft.miraebook.controller;
 
-public class BookController {
+import java.util.ArrayList;
 
+import javax.inject.Inject;
+
+import kr.miraesoft.miraebook.domain.Book;
+import kr.miraesoft.miraebook.service.BookService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/book")
+public class BookController {
+	
+	@Inject
+	private BookService bookService;
+	
+	@RequestMapping(value="write",method=RequestMethod.GET)
+	String write(){
+		return "book/write";
+	}
+	
+	@RequestMapping(value="write",method=RequestMethod.POST)
+	String write(Model model, Book book){
+		model.addAttribute("booklist", new ArrayList());
+		return null;
+	}
 }
