1413618c41a33e2f0c1689aead61eeaacbd24426
soyoung ahn <sosimisy@miraesoft.kr>
2012-02-28 17:15:45 +0900

    author Test @transactional 추가

--- src/test/java/kr/miraesoft/miraebook/repository/AuthorTest.java
+++ src/test/java/kr/miraesoft/miraebook/repository/AuthorTest.java
@@ -10,9 +10,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/applicationContext.xml"})
+@Transactional
 public class AuthorTest {
 	
 	@Autowired
